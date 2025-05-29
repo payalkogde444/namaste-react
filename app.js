a4111b8386710d5f57cd491c1d3e937b35beaef9
payalkogde <payalkogde123@gmail.com>
2025-05-29 16:16:38 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,24 +2,145 @@
 // const root=ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
 
+// import React, { createElement } from "react"
+// import ReactDOM, { createRoot } from "react-dom/client"
 
-const parent= React.createElement("div",{id:"parent"},
-[ React.createElement("div",{id:"child"},
-    [React.createElement("h1",{id:"child"},"I am h1 tag"),
-        React.createElement("h2",{id:"child"},"I am h2 tag")
-    ]), React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{id:"child2"},"I am h1 tag"),
-            React.createElement("h2",{id:"child2"},"I am h2 tag")
-        ]),]
-);
+/**
+ * Header
+ * -logo
+ * -Nav Items
+ * Body
+ *  -search
+ *  -RestraurantContainer
+ * -RestraurantCard
+ *   -Img
+ *    -Name of Res,Star Rating,cuisine,delery tie
+ * Footer
+ * -Copyright
+ * -Links
+ * -Address
+ * -Contact
+ * 
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
-const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+
+const ele= <span>"I am using JSX composition"</span>
+
+const Heading = () => {
+  return(
+  
+  <h1>Namaste react using Jsx</h1>
+
+  
+  );
+   
+};
+
+const number=1589;
+const Title = () => {
+  return (
+    <div>
+       {/* <Heading /> */}
+     {  Heading()}
+       <h2>{3444+1000}</h2>
+       <h2>{console.log("Hey! I am Payal hear")}</h2>
+      <h1>Here I am learning JavaScript</h1>
+     {ele}
+    </div>
+  );
+};                             
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Title());
 
 
 
-// React.createElement("div",{id:"child"},
-//     [React.createElement("h1",{},"I am h1 tag"),
-//         React.createElement("h2",{},"I am h2 tag"),
-//     ])
+
+
+
+
+
+
+
+
+
+
+
+
+// const Header=()=>{
+//   return(
+//   <div className="heading">
+//     <div>
+//       <img   
+//        className="logo "
+//         src="https://img.freepik.com/premium-vector/burger-logo-template_441059-18.jpg"
+//       />
+//     </div>
+//     <div className="nav_item">
+//        <ul>
+//         <li>HOME</li>
+//         <li>ABOUT US</li>
+//         <li>CONTACT US</li>
+//         <li>CARD</li>
+//        </ul>
+//     </div>
+//   </div>
+//   );
+// };
+
+// const RestraurantCard = () => {
+//   return (
+//     <div className="res_card">
+//       <img className="cardimg"alt="" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/cvo1djhbwrgfqd64k0tl" />
+//       <h3>Meghna Foods</h3>  
+//      <h4>Biryani ,North Indian ,Asian</h4>
+//      <h4>4.3 stars</h4>
+//      <h4>38 minutes </h4>
+//     </div>
+//   );
+// };
+// const Body=()=>{
+//    return(
+//     <div className="body">
+//        <div className="searchbar"> 
+//         Search
+//        </div>
+//        <div className="box"> </div>
+//        <div className="res_container"> 
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        <RestraurantCard/>
+//        </div>
+      
+//     </div> 
+//    );
+// }
+
+// const AppLayout=()=>{
+//   return (
+//     <div className="app">
+//     <Header/>
+//     <Body/>
+//     </div>
+//   );
+// };
+  
+// const root=createRoot(document.getElementById("root"));
+// root.render(<AppLayout/>);
